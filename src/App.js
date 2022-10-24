df2484a19bc27fae1770ddf909545e63fe2aad7f
StephanJ98 <diego.jeandon@gmail.com>
2022-10-24 11:42:40 +0200

    :rocket: Main.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Login from "./Pages/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from "./Pages/Login/Login"
+import Main from './Pages/Main/Main';
+import NoValidRoute from "./Pages/NoValidRoute/NoValidRoute"
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/main' element={<Main />} />
+        <Route path="*" element={<NoValidRoute />} />
+      </Routes>
+    </Router>
   );
 }
 
